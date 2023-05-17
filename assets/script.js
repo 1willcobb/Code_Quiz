3a96bfa8bb8c109624fa6077cd772027b3012921
1willcobb <cobb.will@gmail.com>
2023-05-17 14:18:20 -0700

    added some more HTML and created the 'view high scores' function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-//TODO Timeed quiz on JS fundamentals that stores high scores.
+//TODO Timed quiz on JS fundamentals that stores high scores.
 //TODO click the start button a timer starts and given question 1 
 //TODO when i answer i am given the next question
 //TODO when i answer wrong, time is subtracted
 //TODO when all questions are answered OR the timer reaches 0 the game is over
-//TODO when the game is over then i can store my initials in a high score page
-//TODO the high score page can be opened at any time.
+
+
 
 
 function Question(question, answerA, answerB, answerC, answerD, correct) {
@@ -25,10 +25,10 @@ let questions = [];
 
 let q1 = new Question(
     "what is the my name?",
-    "nick",
+    "frank",
     "jack",
     "will",
-    "james",
+    "Julian",
     "will"
 );
 
@@ -37,8 +37,8 @@ function displayQuestion(){
 
 };
 
-let q1_display = document.getElementById("question");
-q1_display.innerText = q1.question;
+let q_display = document.getElementById("question");
+q_display.innerText = q1.question;
 
 
 let answer1 = document.getElementById("1");
@@ -50,21 +50,52 @@ answer3.innerText = q1.answer_choices[2];
 let answer4 = document.getElementById("4");
 answer4.innerText = q1.answer_choices[3];
 
-
-let timer = document.querySelector("#timer_time");
-let start_time = 2;
+let timer = document.querySelector("#timer");
+let timer_time_display = document.querySelector("#timer_time");
+let start_time = 30;
 let question_response = document.querySelector("#question_response");
 
 
 //TODO click the start button a timer starts and given question 1
 let answer_choice = document.querySelector("#answers")
 this.addEventListener("load", function() {
+    timer_time_display.innerText = start_time;
     setInterval(function(){
         if (start_time > 0){
             start_time --;
-            timer.innerText = start_time;
+            timer_time_display.innerText = start_time;
         } else {
             question_response.innerText = "TIMES UP";
         }
     }, 1000);
 });
+
+
+let answers = document.querySelector("#answers")
+
+
+//!! HIGH SCORE PAGE 
+let view_high_scores = document.querySelector("#high_score_link");
+
+view_high_scores.addEventListener("click", function () {
+    let hsPage = document.getElementById("hs_page");
+    if (hsPage.style.display === "none") {
+        hsPage.style.display = "block";
+        answers.style.display = "none";
+        question_response.style.display = "none";
+        timer.style.display = "none";
+        q_display.innerText = "HIGH SCORES"
+    } else {
+        hsPage.style.display = "none";
+        answers.style.display = "flex";
+        question_response.style.display = "block";
+    };
+});
+
+
+//TODO when the game is over then i can store my initials in a high score page
+
+//TODO reset and start over
+
+//* reset */
+
