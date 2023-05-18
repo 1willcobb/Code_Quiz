76eba1a8d980e2c2e1f41af40b99c0fb7f01e91a
1willcobb <cobb.will@gmail.com>
2023-05-18 14:59:54 -0700

    something is wrong wiht my answer logic

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
 //TODO Timed quiz on JS fundamentals that stores high scores.
-//TODO click the start button a timer starts and given question 1 
-//TODO when i answer i am given the next question
-//TODO when i answer wrong, time is subtracted
 //TODO when all questions are answered OR the timer reaches 0 the game is over
 
 //? intro variables
@@ -10,23 +7,36 @@ const intro_p = "It's the game I made from scratch to display questions about JS
 const start_game = document.querySelector("#start");
 const reset_scores = document.querySelector(".reset");
 const main_card = document.querySelector("#main_card");
+const exit_game = document.querySelector("#exit");
 
 //? question display variables
 const q_display = document.getElementById("question");
 const p_display = document.getElementById("p");
 const header = document.querySelector("header");
-let questions = []; //list of questions made
+let qList = []; //list of questions made
+let qList_random = []; //once the questions are jumbled each time they are stored here. 
+var qListIterator;
+var score = 0;
 
 //? Timer variables
 let timer = document.querySelector("#timer"); //the whole timer block
 let timer_time_display = document.querySelector("#timer_time"); // the time displayed
-let game_time = 5; // how long we want to play the game
+let game_time = 100; // how long we want to play the game
 let start_time; // internal counter
 let question_response = document.querySelector("#question_response"); // popup if time is out or question is right/wrong
 let timerID;
 
 //? Block of answers vars
 let answer_block = document.querySelector("#answers");
+let answer1 = document.getElementById("1.1");
+let answer2 = document.getElementById("2.1");
+let answer3 = document.getElementById("3.1");
+let answer4 = document.getElementById("4.1");
+let button1 = document.getElementById("1");
+let button2 = document.getElementById("2");
+let button3 = document.getElementById("3");
+let button4 = document.getElementById("4");
+
 
 //? HS variables
 let view_high_scores = document.querySelector("#high_score_link");
@@ -55,8 +65,9 @@ const q1 = new Question(
     "Refers to the parent",
     "Refers to the child",
     "Refers to a function called 'this'",
-    "Refers to the parent"
+    0
 );
+qList.push(q1);
 
 const q2 = new Question(
     "what is the my name?",
@@ -64,20 +75,37 @@ const q2 = new Question(
     "jack",
     "will",
     "Julian",
-    "will"
+    1
 );
+qList.push(q2);
 
-//TODO click the start button a timer starts and given question 1
+const q3 = new Question(
+    "question 3?",
+    "frank",
+    "jack",
+    "will",
+    "Julian",
+    3
+);
+qList.push(q3);
 
-function evaluateAnswer() {
 
-};
 
-function totalScore(){
-
+function jumbleQuestions(){
+    console.log('entry items', qList);
+    while (qList.length > 0){
+        const randomQ = Math.floor(Math.random() * qList.length);
+        const current_item = qList[randomQ];
+        qList.splice(randomQ, 1);
+        qList_random.push(current_item);
+    }
+    qList = qList_random;
+    qList_random = [];
 }
 
+
 function timerStart() {
+    timerReset();
     timer.style.display = "block";
     timer_time_display.innerText = start_time;
     timerID = setInterval(function(){
@@ -87,6 +115,7 @@ function timerStart() {
         } else {
             question_response.innerText = "TIMES UP";
             clearInterval(timerID);
+            scorePage();
         }
     }, 1000);
 };
@@ -94,7 +123,21 @@ function timerStart() {
 function timerReset() {
     start_time = game_time;
     clearInterval(timerID);
-}
+};
+
+function restartGame(){
+    introPage();
+};
+
+function switchButtons() {
+    if (start_game.style.display === ''){
+        start_game.style.display = "none";
+        exit_game.style.display = "";
+    } else if (exit_game.style.display === ""){
+        start_game.style.display = "";
+        exit_game.style.display = "none";
+    };
+};
 
 function hsPage() {
     main_card.style.display = "none";
@@ -107,45 +150,62 @@ function mainCard() {
     main_card.style.display = "block";
 };
 
-function startGame() {
-    console.log("start clicked");
-    timerReset();
-    timerStart();
-    q_display.innerText = q1.question;
-    p_display.innerText = '';
-    displayAnswers(q1);
-    start_game.innerText = "Restart";
+/* SOMETHING WRONG HERE 2 things dong the same thing
+function gameOn() {
+    if (qListIterator >= 0) {
+        q_display.innerText = qList[qListIterator].question;
+        displayAnswers(qList[qListIterator]);
+        qListIterator --;
+    } else if (qListIterator < 0) {
+        scorePage();
+    }
 };
 
+function evaluateAnswer(event){
+    var element = this.id
+    console.log(qList[qListIterator].correct)
+    if (element === qList[qListIterator].correct) {
+        question_response.innerText = "CORRECT";
+        score ++;
+        gameOn();
+    } else {
+        question_response.innerText = "WRONG, Lost 5 Sec";
+        start_time -= 5;
+        gameOn()
+    }
+};
+*/
+
+function scorePage() {
+    q_display.innerText = "Final Score"
+    p_display.innerText = score;
+    question_response.innerText = '';
+    hideAnswers();
+}
+
+
 function resetAll() {
-    
+    score = 0;
 };
 
 function introPage() {
     mainCard();
     hideAnswers();
     timerReset()
+    score = 0;
+    qListIterator = qList.length - 1;
     timer.style.display = "none";
     question_response.innerText = "";
     q_display.innerText = intro_txt;
     p_display.innerText = intro_p;
-
 };
 
-function displayQuestion() {
-    q_display.innerText = q1.question;
-    displayAnswers(q1);
-};
 
 function displayAnswers(q) {
     showAnswers()
-    let answer1 = document.getElementById("1");
     answer1.innerText = q.answer_choices[0];
-    let answer2 = document.getElementById("2");
     answer2.innerText = q.answer_choices[1];
-    let answer3 = document.getElementById("3");
     answer3.innerText = q.answer_choices[2];
-    let answer4 = document.getElementById("4");
     answer4.innerText = q.answer_choices[3];
 };
 
@@ -169,9 +229,28 @@ view_high_scores.addEventListener("click", function() {
 
 restart.addEventListener("click", introPage);
 
-start_game.addEventListener("click", startGame);
+start_game.addEventListener("click", function(){
+    jumbleQuestions();
+    switchButtons();
+    timerStart();
+    gameOn();
+});
 
 reset_scores.addEventListener("click", resetAll);
+
+exit_game.addEventListener("click",function(){
+    switchButtons();
+    introPage();
+});
+
+
+
+
+button1.addEventListener("click", evaluateAnswer);
+button2.addEventListener("click", evaluateAnswer);
+button3.addEventListener("click", evaluateAnswer);
+button4.addEventListener("click", evaluateAnswer);
+
 
 
 
