467c6ff1162a940e4985e8424444c10e9741e0b7
1willcobb <cobb.will@gmail.com>
2023-05-18 07:29:18 -0700

    cleanred up JS to be more function and OOP

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,111 @@
 //TODO when i answer wrong, time is subtracted
 //TODO when all questions are answered OR the timer reaches 0 the game is over
 
+//? intro variables
+const intro_txt = "Welcome to the JavaScript Code Quiz!";
+const intro_p = "It's the game I made from scratch to display questions about JS";
+const start_game = document.querySelector("#start");
+start_game.addEventListener("click", start());
+const reset_scores = document.querySelector(".reset");
+reset_scores.addEventListener("click", resetAll());
+const main_card = document.querySelector("#main_card");
+
+//? question display variables
+const q_display = document.getElementById("question");
+const p_display = document.getElementById("p");
+const header = document.querySelector("header");
+let questions = []; //list of questions made
+
+//? Timer variables
+let timer = document.querySelector("#timer"); //the whole timer block
+let timer_time_display = document.querySelector("#timer_time"); // the time displayed
+let start_time = 5; // internal counter
+let question_response = document.querySelector("#question_response"); // popup if time is out or question is right/wrong
+
+//? Block of answers vars
+let answer_block = document.querySelector("#answers");
+
+//? HS variables
+let view_high_scores = document.querySelector("#high_score_link");
+let hs_page = document.querySelector("#hs_page");
+let restart = document.querySelector("#restart");
 
 
 
+//TODO click the start button a timer starts and given question 1
+
+function evaluateAnswer() {
+
+};
+
+function timerStart() {
+    timer_time_display.innerText = start_time;
+    setInterval(function(){
+        if (start_time > 0){
+            start_time --;
+            timer_time_display.innerText = start_time;
+        } else {
+            question_response.innerText = "TIMES UP";
+        }
+    }, 1000);
+};
+
+
+
+function mainCard() {
+    
+};
+
+function start() {
+    
+};
+
+function resetAll() {
+    
+};
+
+function introPage() {
+    q_display.innerText = intro_txt;
+    p_display.innerText = intro_p;
+};
+
+function displayQuestion() {
+    q_display.innerText = q1.question;
+    displayAnswers(q1);
+};
+
+function displayAnswers(q) {
+    let answer1 = document.getElementById("1");
+    answer1.innerText = q.answer_choices[0];
+    let answer2 = document.getElementById("2");
+    answer2.innerText = q.answer_choices[1];
+    let answer3 = document.getElementById("3");
+    answer3.innerText = q.answer_choices[2];
+    let answer4 = document.getElementById("4");
+    answer4.innerText = q.answer_choices[3];
+};
+
+
+
+//? Listener Section
+view_high_scores.addEventListener("click", function() {
+    console.log("view HS pressed");
+    if (main_card.style.display == "block") {
+        main_card.style.display = "none";
+        answer_block.style.display = "none";
+        hs_page.style.display = "block";
+    };
+});
+
+restart.addEventListener("click", function() {
+    hs_page.style.display = "none";
+    main_card.style.display = "block";
+    introPage();
+});
+
+
+//? Question Creation Section
+//TODO Maybe add a feature for user input to add questions to the game.
 function Question(question, answerA, answerB, answerC, answerD, correct) {
     this.question = question;
     this.answer_choices = [answerA, answerB, answerC, answerD];
@@ -21,9 +123,7 @@ function Question(question, answerA, answerB, answerC, answerD, correct) {
     }
 }
 
-let questions = [];
-
-let q1 = new Question(
+const q1 = new Question(
     "what is the my name?",
     "frank",
     "jack",
@@ -33,69 +133,10 @@ let q1 = new Question(
 );
 
 
-function displayQuestion(){
-
-};
-
-let q_display = document.getElementById("question");
-q_display.innerText = q1.question;
-
-
-let answer1 = document.getElementById("1");
-answer1.innerText = q1.answer_choices[0];
-let answer2 = document.getElementById("2");
-answer2.innerText = q1.answer_choices[1];
-let answer3 = document.getElementById("3");
-answer3.innerText = q1.answer_choices[2];
-let answer4 = document.getElementById("4");
-answer4.innerText = q1.answer_choices[3];
-
-let timer = document.querySelector("#timer");
-let timer_time_display = document.querySelector("#timer_time");
-let start_time = 30;
-let question_response = document.querySelector("#question_response");
-
-
-//TODO click the start button a timer starts and given question 1
-let answer_choice = document.querySelector("#answers")
-this.addEventListener("load", function() {
-    timer_time_display.innerText = start_time;
-    setInterval(function(){
-        if (start_time > 0){
-            start_time --;
-            timer_time_display.innerText = start_time;
-        } else {
-            question_response.innerText = "TIMES UP";
-        }
-    }, 1000);
-});
-
-
-let answers = document.querySelector("#answers")
-
-
-//!! HIGH SCORE PAGE 
-let view_high_scores = document.querySelector("#high_score_link");
-
-view_high_scores.addEventListener("click", function () {
-    let hsPage = document.getElementById("hs_page");
-    if (hsPage.style.display === "none") {
-        hsPage.style.display = "block";
-        answers.style.display = "none";
-        question_response.style.display = "none";
-        timer.style.display = "none";
-        q_display.innerText = "HIGH SCORES"
-    } else {
-        hsPage.style.display = "none";
-        answers.style.display = "flex";
-        question_response.style.display = "block";
-    };
-});
 
 
 //TODO when the game is over then i can store my initials in a high score page
 
-//TODO reset and start over
 
-//* reset */
 
+introPage();
